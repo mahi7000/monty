429287fc52e74172e2eb88f0f127ebe150aea177
mahi7000 <mahlet7000@gmail.com>
2024-02-23 01:01:18 -0800

    monty

--- monty.c
+++ monty.c
@@ -16,14 +16,14 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		printf("Error: Can't open file <file>\n");
+		fprintf(stderr, "Error: Can't open file <file>\n");
 		exit(EXIT_FAILURE);
 	}
 
